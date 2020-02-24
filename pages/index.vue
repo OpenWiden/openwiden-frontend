c4b5a735a3004769a6280836c9516aa8ddeaa486
Mikhail <mcheremuhin@ostrovok.ru>
2020-02-24 19:30:25 +0300

    Add MainScreen (Intro) component to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,12 @@
 <template>
-  <div class="container"></div>
+  <main-screen />
 </template>
+
+<script>
+import MainScreen from '@/src/components/MainScreen/MainScreen';
+export default {
+  components: {
+    MainScreen,
+  },
+};
+</script>
