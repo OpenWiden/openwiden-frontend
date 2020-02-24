0e963a1afb4c03c699396d5d36401c806e6bcc48
Mikhail <mcheremuhin@ostrovok.ru>
2020-02-24 19:29:01 +0300

    Add basic markup of Header and Intro components
    - Add Text component for page texts and titles

--- src/components/TopHeader/TopHeader.vue
+++ src/components/TopHeader/TopHeader.vue
@@ -1,49 +1,37 @@
 <template>
-  <header class="header">
-    <div class="header__wrapper">
+  <header :class="styles.header">
+    <div :class="styles.headerWrapper">
       <nuxt-link to="/" class="logo">
         <logo width="56" height="21" />
-        <span class="logo__text">Open Widen</span>
+        <span :class="styles.logoText">Open Widen</span>
       </nuxt-link>
+
+      <nav :class="styles.headerNav" role="navigation">
+        <ul :class="styles.headerNavList">
+          <li :class="styles.headerNavItem">
+            <a :class="styles.headerNavLink" href="#">Sign In</a>
+          </li>
+          <li :class="styles.headerNavItem">
+            <a :class="styles.headerNavLink" href="#">Sign Up</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   </header>
 </template>
 
 <script>
+import styles from './TopHeader.css?module';
 import Logo from '@/src/components/Logo/Logo.vue';
 
 export default {
   components: {
     Logo,
   },
+  computed: {
+    styles() {
+      return styles;
+    },
+  },
 };
 </script>
-
-<style>
-.header {
-  display: flex;
-  align-items: center;
-  min-height: 51px;
-  background-color: var(--dark-theme-secondary-bg);
-  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.12), 0px 4px 4px rgba(0, 0, 0, 0.24);
-}
-
-.header__wrapper {
-  display: flex;
-  justify-content: space-between;
-  padding: 0 30px;
-  max-width: 1200px;
-}
-
-.logo {
-  display: flex;
-  align-items: center;
-}
-
-.logo__text {
-  margin: 4px 0 0 10px;
-  font-size: 20px;
-  font-weight: bold;
-  color: var(--dark-theme-primary-typo);
-}
-</style>
