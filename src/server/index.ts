be90a60a084f2c4c454c1b65e96feef05e45c68a
Mikhail <mcheremuhin@ostrovok.ru>
2020-03-15 20:47:41 +0300

    Change server script extention

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,30 +4,31 @@ const { Nuxt, Builder } = require('nuxt');
 const app = express();
 
 // Import and Set Nuxt.js options
+const cookieParser = require('cookie-parser');
 const config = require('../../nuxt.config.js');
+
 config.dev = process.env.NODE_ENV !== 'production';
 
 async function start() {
-  // Init Nuxt.js
   const nuxt = new Nuxt(config);
 
   const { host, port } = nuxt.options.server;
 
   await nuxt.ready();
-  // Build only in dev mode
+
   if (config.dev) {
     const builder = new Builder(nuxt);
     await builder.build();
   }
-  // Give nuxt middleware to express
-  app.use(nuxt.render);
+  app.use(cookieParser(), nuxt.render);
 
-  // Listen the server
-  app.listen(port, host);
+  app.listen(port, (err: any) => {
+    if (err) throw err;
 
-  consola.ready({
-    message: `[ 🚀 ] Server listening on http://${host}:${port}`,
-    badge: true,
+    consola.ready({
+      message: `[ 🚀 ] Server listening on http://${host}:${port}`,
+      badge: true,
+    });
   });
 }
 
