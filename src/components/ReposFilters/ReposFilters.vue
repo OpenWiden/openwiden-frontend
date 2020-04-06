7c984646c73c9f6ac821f90e7db18aec6aa9862d
Mikhail <mcheremuhin@ostrovok.ru>
2020-04-06 08:32:53 +0300

    Add repos filters component

---
+++ src/components/ReposFilters/ReposFilters.vue
@@ -0,0 +1,43 @@
+<template>
+  <div :class="styles.filters">
+    <ul :class="styles.filtersList">
+      <li :class="styles.filterItem">
+        <the-select
+          :options="languages"
+          label="Language"
+          placeholder="Choose language..."
+        />
+      </li>
+      <li :class="styles.filterItem">
+        <the-button
+          :class="styles.searchButton"
+          title="Search repositories"
+          type="button"
+        >
+          Search
+        </the-button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import styles from './ReposFilters.css?module';
+import TheSelect from '@/src/components/TheSelect/TheSelect';
+import TheButton from '@/src/components/TheButton/TheButton';
+
+export default {
+  components: {
+    TheSelect,
+    TheButton,
+  },
+  data() {
+    return { languages: ['Python', 'Go', 'JavaScript'] };
+  },
+  computed: {
+    styles() {
+      return styles;
+    },
+  },
+};
+</script>
