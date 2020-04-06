47a1bc699652e78be49663a007c394c2200bdb50
Mikhail <mcheremuhin@ostrovok.ru>
2020-04-06 08:49:24 +0300

    Refactoring of ReposList component

--- src/components/ReposList/ReposList.vue
+++ src/components/ReposList/ReposList.vue
@@ -1,51 +1,48 @@
 <template>
   <section :class="styles.repos">
     <div :class="styles.wrapper">
-      <the-text :class="styles.title" tag="h2"
-        >List of projects on GitHub that need your help</the-text
-      >
+      <the-text :class="styles.title" tag="h2">
+        List of projects on GitHub that need your help
+      </the-text>
 
-      <ul :class="styles.reposList">
-        <li :class="styles.repoItem">
-          <div :class="styles.repoHeader">
-            <the-text tag="p">Gviddo / Python</the-text>
-          </div>
-          <div :class="styles.repoDescription">
-            <the-text tag="p">
-              Is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since
-              the 1500s, when an unknown printer took a galley of type and
-              scrambled it to make a type specimen book.
-            </the-text>
-          </div>
-          <footer :class="styles.repoFooter">
-            <ul :class="styles.statsList">
-              <li :class="styles.statItem">
-                <icon-star
-                  :class="styles.icon"
-                  width="16"
-                  height="16"
-                  aria-hidden="true"
-                />
-                <the-text :class="styles.statText" tag="p">
-                  25k
-                </the-text>
-              </li>
-              <li :class="styles.statItem">
-                <icon-issue
-                  :class="styles.icon"
-                  width="16"
-                  height="16"
-                  aria-hidden="true"
-                />
-                <the-text :class="styles.statText" tag="p">
-                  1004
-                </the-text>
-              </li>
-            </ul>
-          </footer>
-        </li>
-      </ul>
+      <repos-filters />
+
+      <div v-if="areReposLoading" :class="styles.reposWrapper">
+        <ul :class="styles.reposList">
+          <li :class="styles.repoItem">
+            <repo-card-skeleton />
+          </li>
+          <li :class="styles.repoItem">
+            <repo-card-skeleton />
+          </li>
+          <li :class="styles.repoItem">
+            <repo-card-skeleton />
+          </li>
+          <li :class="styles.repoItem">
+            <repo-card-skeleton />
+          </li>
+          <li :class="styles.repoItem">
+            <repo-card-skeleton />
+          </li>
+          <li :class="styles.repoItem">
+            <repo-card-skeleton />
+          </li>
+        </ul>
+      </div>
+
+      <div v-else-if="!areReposLoading && error" :class="styles.reposWrapper">
+        <the-text :class="styles.title" tag="p">
+          An error occurred while loading the repositories
+        </the-text>
+      </div>
+
+      <div v-else :class="styles.reposWrapper">
+        <ul :class="styles.reposList">
+          <li v-for="repo in repos" :key="repo.name" :class="styles.repoItem">
+            <repo-card :repository="repo" />
+          </li>
+        </ul>
+      </div>
     </div>
   </section>
 </template>
@@ -53,19 +50,42 @@
 <script>
 import styles from './ReposList.css?module';
 import TheText from '@/src/components/TheText/TheText';
-import IconStar from '@/src/components/Icons/IconStar';
-import IconIssue from '@/src/components/Icons/IconIssue';
+import RepoCard from '@/src/components/RepoCard/RepoCard';
+import ReposFilters from '@/src/components/ReposFilters/ReposFilters';
+import RepoCardSkeleton from '@/src/components/RepoCardSkeleton/RepoCardSkeleton';
 
 export default {
   components: {
     TheText,
-    IconStar,
-    IconIssue,
+    RepoCard,
+    RepoCardSkeleton,
+    ReposFilters,
+  },
+  data() {
+    return {
+      areReposLoading: true,
+      reposCount: 0,
+      repos: null,
+      error: false,
+    };
   },
   computed: {
     styles() {
       return styles;
     },
+  },
+  beforeMount() {
+    this.$axios
+      .$get('/api/v1/repositories/')
+      .then(({ count, results }) => {
+        this.repos = results;
+        this.reposCount = count;
+        this.areReposLoading = false;
+      })
+      .catch((err) => {
+        this.error = err;
+        this.areReposLoading = false;
+      });
   },
 };
 </script>
