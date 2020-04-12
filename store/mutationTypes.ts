1e704dc036de1070f67b0e55067c81f7bcbcf73e
Mikhail <mcheremuhin@ostrovok.ru>
2020-04-12 20:32:57 +0300

    Add filters store mutation

--- store/mutationTypes.ts
+++ store/mutationTypes.ts
@@ -1,6 +1,14 @@
 export enum MUTATIONS {
+  /** Auth */
   SET_AUTH = 'SET_AUTH',
   RESET_AUTH = 'RESET_AUTH',
+
+  /** User */
   SET_USER = 'SET_USER',
+
+  /** Provider */
   SET_PROVIDER = 'SET_PROVIDER',
+
+  /** Filters */
+  SET_FILTER = 'SET_FILTER',
 }
