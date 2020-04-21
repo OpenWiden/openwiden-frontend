a84857064fdfc295a7fe92b38ada60e630eedae9
Mikhail <mcheremuhin@ostrovok.ru>
2020-04-21 23:41:04 +0300

    Remove commented code

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,29 +1,27 @@
 import consola from 'consola';
 
 export default function (context: any) {
-  const { $axios, req } = context;
-
-  if (req?.cookies?.auth) {
-    $axios.setHeader('Authorization', `JWT ${req.cookies.auth}`);
-  }
+  const { $axios, redirect } = context;
 
   $axios.onRequest((config: any) => {
     consola.info(`[ ${config.method.toUpperCase()} ] ${config.url}`);
   });
 
   $axios.onError((error: any) => {
-    const { status } = error?.response;
+    if (error.response) {
+      const { status } = error.response;
 
-    if (status) {
-      switch (status) {
-        case 401:
-          break;
-        case 404:
-          // redirect('/404');
-          break;
-        case 500:
-          // redirect('/500');
-          break;
+      if (status) {
+        switch (status) {
+          case 401:
+            break;
+          case 404:
+            redirect('/404');
+            break;
+          case 500:
+            redirect('/500');
+            break;
+        }
       }
     }
   });
