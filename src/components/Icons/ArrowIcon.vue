0cd0aa06d679a4115b28bb76e0878f7effa47b3c
Mikhail <mcheremuhin@ostrovok.ru>
2020-04-24 18:56:05 +0300

    Refactor clean button

--- src/components/Icons/ArrowIcon.vue
+++ src/components/Icons/ArrowIcon.vue
@@ -1,14 +1,7 @@
 <template>
-  <svg
-    with="16"
-    height="16"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 16 16"
-  >
+  <svg fill="#ffffff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
     <path
       d="M7.274 4.302L2.362 9.214c-.4.399-.4 1.052 0 1.452l.138.139a1.032 1.032 0 001.454 0L8 6.758l4.048 4.048a1.03 1.03 0 001.451 0l.138-.141c.4-.4.4-1.053 0-1.453L8.726 4.301a1.03 1.03 0 00-1.451 0"
-      fill="#ffffff"
     />
   </svg>
 </template>
