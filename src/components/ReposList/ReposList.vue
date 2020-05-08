a3197ff5a0df40dab8c474b4ce41e5935b63376e
Mikhail <mcheremuhin@ostrovok.ru>
2020-05-08 17:49:37 +0300

    Edit ReposList

--- src/components/ReposList/ReposList.vue
+++ src/components/ReposList/ReposList.vue
@@ -5,7 +5,7 @@
         List of projects on GitHub that need your help
       </the-text>
 
-      <repos-filters />
+      <repos-filters :on-search="getRepos" />
 
       <div v-if="areReposLoading" :class="styles.reposWrapper">
         <ul :class="styles.reposList">
@@ -21,9 +21,15 @@
         </the-text>
       </div>
 
+      <div v-else-if="repos.length === 0" :class="styles.reposWrapper">
+        <the-text :class="styles.title" tag="p">
+          There are no suitable repositories to show
+        </the-text>
+      </div>
+
       <div v-else :class="styles.reposWrapper">
         <ul :class="styles.reposList">
-          <li v-for="repo in items" :key="repo.name" :class="styles.repoItem">
+          <li v-for="repo in repos" :key="repo.name" :class="styles.repoItem">
             <repo-card :repository="repo" />
           </li>
         </ul>
@@ -34,51 +40,12 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import config from '../../../config';
 import styles from './ReposList.css?module';
 import TheText from '@/src/components/TheText/TheText.vue';
 import RepoCard from '@/src/components/RepoCard/RepoCard.vue';
 import ReposFilters from '@/src/components/ReposFilters/ReposFilters.vue';
 import RepoCardSkeleton from '@/src/components/RepoCardSkeleton/RepoCardSkeleton.vue';
-import { Filter, Filters } from '@/src/interfaces/Filters';
-import { getObjectKeys } from '@/src/lib/getObjectKeys';
 import { Repository } from '@/src/interfaces/Repository';
-
-const filtrators: {
-  [k: string]: (repos: Repository[], filterValue: any) => Repository[];
-} = {
-  [Filter.STAR_COUNT]: (repos, filterValue) => {
-    if (!filterValue) return repos;
-    return repos;
-  },
-  [Filter.OPEN_ISSUES_COUNT]: (repos, filterValue) => {
-    if (!filterValue) return repos;
-    return repos;
-  },
-  [Filter.FORKS_COUNT]: (repos, filterValue) => {
-    if (!filterValue) return repos;
-    return repos;
-  },
-  [Filter.VERSION_CONTROL_SERVICE]: (repos, filterValue) => {
-    if (!filterValue) return repos;
-    return repos;
-  },
-  [Filter.CREATED_AT]: (repos, filterValue) => {
-    if (!filterValue) return repos;
-    return repos;
-  },
-  [Filter.UPDATED_AT]: (repos, filterValue) => {
-    if (!filterValue) return repos;
-    return repos;
-  },
-  [Filter.PROGRAMMING_LANGUAGE]: (repos, filterValue) => {
-    if (!filterValue) return repos;
-
-    return repos.filter(
-      (repo) => repo.programming_language.name === filterValue.name
-    );
-  },
-};
 
 @Component({
   components: {
@@ -94,10 +61,14 @@ export default class ReposList extends Vue {
   repos: Repository[] | null = null;
   error: boolean = false;
 
-  created() {
-    this.$axios
-      .$get(config.urls.repositories)
-      .then(({ count, results }) => {
+  async created() {
+    await this.getRepos();
+  }
+
+  public getRepos() {
+    this.$api
+      .getRepositories()
+      .then(({ results, count }) => {
         this.repos = results;
         this.reposCount = count;
         this.areReposLoading = false;
@@ -110,17 +81,6 @@ export default class ReposList extends Vue {
 
   get styles() {
     return styles;
-  }
-
-  get items() {
-    const { filters }: { filters: Filters } = this.$store.state;
-    const filtersKeys = getObjectKeys(filters);
-
-    if (!this.repos) return null;
-
-    return filtersKeys.reduce((filteredRepos: Repository[], filter: Filter) => {
-      return filtrators[filter](filteredRepos, filters[filter]);
-    }, this.repos);
   }
 }
 </script>
