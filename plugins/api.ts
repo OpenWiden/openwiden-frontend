2c49e77da1e6e5127530bcc598c111c24c45ead2
Mikhail <mcheremuhin@ostrovok.ru>
2020-05-08 18:19:12 +0300

    Edit api

--- plugins/api.ts
+++ plugins/api.ts
@@ -2,16 +2,18 @@ import { Context } from '@nuxt/types';
 import { NuxtAxiosInstance } from '@nuxtjs/axios';
 import config from '@/config';
 
-import { User } from '@/src/interfaces/User/User';
+import { UserData, User } from '@/src/interfaces/User/User';
 import { AuthorizationTokens } from '@/src/interfaces/AuthorizationTokens';
-import { Filter } from '@/src/interfaces/Filters';
 import { AppState } from '@/src/interfaces/AppState';
-import { ProgrammingLanguage } from '@/src/interfaces/ProgrammingLanguage';
-import { Repositories } from '@/src/interfaces/Repositories/Repositories';
+import {
+  RepositoriesData,
+  Repositories,
+} from '@/src/interfaces/Repositories/Repositories';
 import userResolver from '@/src/interfaces/User/resolver';
 import repositoryResolver from '@/src/interfaces/Repository/resolver';
+import { ProgrammingLanguage } from '@/src/interfaces/ProgrammingLanguage';
 
-import { getObjectKeys } from '@/src/lib/getObjectKeys';
+import setFiltersQueryParams from '@/src/lib/setFiltersQueryParams';
 
 interface Store {
   state: AppState;
@@ -19,7 +21,7 @@ interface Store {
 
 const apiCreator = ($axios: NuxtAxiosInstance, store: Store) => ({
   getUser(authToken: string): Promise<User> {
-    const user = $axios.$get(config.urls.user, {
+    const user = $axios.$get<UserData>(config.urls.user, {
       headers: {
         Authorization: `JWT ${authToken}`,
       },
@@ -42,32 +44,22 @@ const apiCreator = ($axios: NuxtAxiosInstance, store: Store) => ({
     const { filters } = store.state;
     const url = new URL(urls.repositories, $axios.defaults.baseURL);
 
-    if (filters) {
-      getObjectKeys(filters).forEach((filter) => {
-        let filterValue = filters[filter];
+    setFiltersQueryParams(url, filters);
 
-        if (!filterValue) return;
-
-        if (filter === Filter.PROGRAMMING_LANGUAGE) {
-          filterValue = (filterValue as ProgrammingLanguage).id;
-        }
-
-        url.searchParams.set(String(filter).toLowerCase(), String(filterValue));
-      });
-    }
-
-    return $axios.$get(url.href).then((data) => ({
+    return $axios.$get<RepositoriesData>(url.href).then((data) => ({
       ...data,
       results: data.results.map(repositoryResolver),
     }));
   },
 
-  authorizeUser(code: string, state: string): Promise<AuthorizationTokens> {
-    return $axios.$get(`auth/complete/github/?code=${code}&state=${state}`);
+  getProgrammingLanguages(): Promise<ProgrammingLanguage[]> {
+    return $axios.$get<ProgrammingLanguage[]>(config.urls.programmingLanguages);
   },
 
-  getProgrammingLanguages(): Promise<any> {
-    return $axios.$get(config.urls.programmingLanguages);
+  authorizeUser(code: string, state: string): Promise<AuthorizationTokens> {
+    return $axios.$get<AuthorizationTokens>(
+      `auth/complete/${store.state.provider}/?code=${code}&state=${state}`
+    );
   },
 });
 
