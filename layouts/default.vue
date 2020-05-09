8697bf22b735678d8f1ad4ced17b5abe321f31e6
Mikhail <mcheremuhin@ostrovok.ru>
2020-05-09 18:19:06 +0300

    Add basic repo page with routing

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <top-header />
-    <main role="main">
+    <main class="main" role="main">
       <nuxt />
     </main>
     <the-footer />
@@ -46,3 +46,9 @@ export default {
   },
 };
 </script>
+
+<style>
+.main {
+  min-height: 100vh;
+}
+</style>
