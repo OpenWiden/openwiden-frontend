3169843bb7d31685633bd39b534e0ecd935f70cb
Mikhail <mcheremuhin@ostrovok.ru>
2020-05-11 12:53:15 +0300

    Add RepoLang component

---
+++ src/components/RepoLang/RepoLang.vue
@@ -0,0 +1,44 @@
+<template>
+  <div v-if="language.icon">
+    <img
+      v-if="language"
+      :src="language.icon"
+      :alt="language.name"
+      :width="width"
+      :height="height"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    programmingLanguage: {
+      type: [Object, null],
+      default: () => null,
+    },
+    width: {
+      type: String,
+      default: '24',
+    },
+    height: {
+      type: String,
+      default: '24',
+    },
+  },
+  computed: {
+    language() {
+      const { name } = this.$props.programmingLanguage;
+
+      try {
+        return {
+          icon: require(`@/assets/svgs/prog-languages/${name}.svg`),
+          name,
+        };
+      } catch (err) {
+        return null;
+      }
+    },
+  },
+};
+</script>
