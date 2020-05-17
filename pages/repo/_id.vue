d61492268ccbe12242c0b5a5c6258aa49989a2d7
Mikhail <mcheremuhin@ostrovok.ru>
2020-05-17 17:35:36 +0300

    Add Pagination component

--- pages/repo/_id.vue
+++ pages/repo/_id.vue
@@ -1,143 +1,141 @@
 <template>
-  <section :class="styles.wrapper">
-    <div v-if="repository.loadingStatus === DATA_STATUS.LOADING">
-      <header :class="styles.header">
-        <div :class="styles.texts">
-          <the-text :class="styles.title" tag="h1">
-            <skeleton :class="styles.title" is-line />
-          </the-text>
+  <section :class="styles.repo">
+    <div :class="styles.wrapper">
+      <div v-if="repository.loadingStatus === DATA_STATUS.LOADING">
+        <header :class="styles.header">
+          <div :class="styles.texts">
+            <the-text :class="styles.title" tag="h1">
+              <skeleton :class="styles.title" is-line />
+            </the-text>
 
-          <div :style="{ margin: '10px 0' }" :class="styles.stats">
-            <skeleton :style="{ height: '24px' }" is-line />
-          </div>
-
-          <skeleton
-            v-for="index in 3"
-            :key="index"
-            :class="styles.description"
-            :style="{ width: `${24 - index * 4}em` }"
-            is-line
-          />
-        </div>
-
-        <div>
-          <skeleton :style="{ width: '34px', height: '34px' }" is-circle />
-        </div>
-      </header>
-    </div>
-
-    <div v-else-if="repository.loadingStatus === DATA_STATUS.LOADED">
-      <header :class="styles.header">
-        <div :class="styles.texts">
-          <the-text :class="styles.title" tag="h1">
-            {{ repository.data.name }}
-          </the-text>
-
-          <div :class="styles.stats">
-            <repo-stats v-bind="repository.data" />
-          </div>
-
-          <the-text :class="styles.description" tag="p">
-            {{ repository.data.description }}
-          </the-text>
-        </div>
-
-        <div>
-          <repo-lang
-            :programming-language="repository.data.programmingLanguage"
-            width="34"
-            height="34"
-          />
-        </div>
-      </header>
-    </div>
-
-    <div :class="styles.issuesBlock">
-      <the-text :class="styles.issuesTitle" tag="h2">
-        Issues
-      </the-text>
-
-      <div>
-        <ul
-          v-if="issues.loadingStatus === DATA_STATUS.LOADING"
-          :class="[styles.issuesList]"
-        >
-          <li
-            v-for="index in issuesSkeletons"
-            :key="index"
-            :class="[styles.issue, styles.loadingIssue]"
-          >
-            <div :class="styles.issueLink">
-              <icon-issue :class="styles.issueIcon" width="16" height="16" />
-
-              <div :class="styles.issueDescription">
-                <skeleton :style="{ width: '14em' }" is-line />
-
-                <skeleton :class="styles.info" is-line />
-              </div>
+            <div :style="{ margin: '10px 0' }" :class="styles.stats">
+              <skeleton :style="{ height: '24px' }" is-line />
             </div>
-          </li>
-        </ul>
 
-        <div v-if="issues.loadingStatus === DATA_STATUS.LOADED">
-          <ul :class="styles.issuesList">
+            <skeleton
+              v-for="index in 3"
+              :key="index"
+              :class="styles.description"
+              :style="{ maxWidth: `${24 - index * 4}em` }"
+              is-line
+            />
+          </div>
+
+          <div>
+            <skeleton
+              :class="styles.progLangIcon"
+              :style="{ width: '30px', height: '30px' }"
+              is-circle
+            />
+          </div>
+        </header>
+      </div>
+
+      <div v-else-if="repository.loadingStatus === DATA_STATUS.LOADED">
+        <header :class="styles.header">
+          <div :class="styles.texts">
+            <the-text :class="styles.title" tag="h1">
+              {{ repository.data.name }}
+            </the-text>
+
+            <div :class="styles.stats">
+              <repo-stats :class="styles.statsList" v-bind="repository.data" />
+            </div>
+
+            <the-text :class="styles.description" tag="p">
+              {{ repository.data.description }}
+            </the-text>
+          </div>
+
+          <repo-lang
+            :class="styles.progLangIcon"
+            :programming-language="repository.data.programmingLanguage"
+            width="30"
+            height="30"
+          />
+        </header>
+      </div>
+
+      <div :class="styles.issuesBlock">
+        <the-text :class="styles.issuesTitle" tag="h2">
+          Issues
+        </the-text>
+
+        <div>
+          <ul
+            v-if="issues.loadingStatus === DATA_STATUS.LOADING"
+            :class="[styles.issuesList]"
+          >
             <li
-              v-for="issue in issues.data"
-              :key="issue.id"
-              :class="styles.issue"
+              v-for="index in issuesSkeletons"
+              :key="index"
+              :class="[styles.issue, styles.loadingIssue]"
             >
-              <a
-                :class="styles.issueLink"
-                :href="issue.url"
-                target="_blank"
-                title="Open the issue"
-              >
+              <div :class="styles.issueLink">
                 <icon-issue :class="styles.issueIcon" width="16" height="16" />
 
                 <div :class="styles.issueDescription">
-                  <the-text tag="p">
-                    {{ issue.title }}
-                  </the-text>
+                  <skeleton :style="{ width: '14em' }" is-line />
 
-                  <the-text :class="styles.info" tag="p">
-                    Opened {{ getDateDifference(issue.createdAt) }} days ago •
-                    updated {{ getDateDifference(issue.updatedAt) }} day ago
-                  </the-text>
+                  <skeleton :class="styles.info" is-line />
                 </div>
-              </a>
+              </div>
             </li>
           </ul>
 
-          <div :class="styles.pagination">
-            <the-button
-              :on-click="() => getRepoIssues(pagination.previous)"
-              :class="styles.paginationButton"
-              :disabled="!Boolean(pagination.previous)"
-              title="Previous"
-            >
-              Previous
-            </the-button>
-            <the-button
-              :class="styles.paginationButton"
-              :disabled="!Boolean(pagination.next)"
-              :on-click="() => getRepoIssues(pagination.next)"
-              title="Next"
-            >
-              Next
-            </the-button>
+          <div v-if="issues.loadingStatus === DATA_STATUS.LOADED">
+            <ul :class="styles.issuesList">
+              <li
+                v-for="issue in issues.data"
+                :key="issue.id"
+                :class="styles.issue"
+              >
+                <a
+                  :class="styles.issueLink"
+                  :href="issue.url"
+                  :title="`Link to issue. ${issue.title}`"
+                  target="_blank"
+                >
+                  <icon-issue
+                    :class="styles.issueIcon"
+                    width="16"
+                    height="16"
+                  />
+
+                  <div :class="styles.issueDescription">
+                    <the-text tag="p">
+                      {{ issue.title }}
+                    </the-text>
+
+                    <the-text :class="styles.info" tag="p">
+                      Opened {{ getDateDifference(issue.createdAt) }} days ago •
+                      updated {{ getDateDifference(issue.updatedAt) }} day ago
+                    </the-text>
+                  </div>
+                </a>
+              </li>
+            </ul>
+
+            <pagination
+              :pagination="pagination"
+              :wrapper-class="styles.pagination"
+              :on-prev-click="() => getRepoIssues(pagination.previous)"
+              :on-next-click="() => getRepoIssues(pagination.next)"
+            />
           </div>
-        </div>
 
-        <div v-if="issues.loadingStatus === DATA_STATUS.IDLE">
-          <the-text tag="p">
-            This repository doesn't have issues. Lucky!
-          </the-text>
-        </div>
+          <div v-if="issues.loadingStatus === DATA_STATUS.IDLE">
+            <the-text tag="p">
+              This repository doesn't have issues. Lucky!
+            </the-text>
+          </div>
 
-        <div v-if="issues.loadingStatus === DATA_STATUS.FAILED">
-          <the-text tag="p">
-            Error occured while we tried to fetch issues. Please try again later
-          </the-text>
+          <div v-if="issues.loadingStatus === DATA_STATUS.FAILED">
+            <the-text tag="p">
+              Error occured while we tried to fetch issues. Please try again
+              later
+            </the-text>
+          </div>
         </div>
       </div>
     </div>
@@ -147,7 +145,7 @@
 <script>
 import styles from './repo.css?module';
 import TheText from '@/src/components/TheText/TheText';
-import TheButton from '@/src/components/TheButton/TheButton';
+import Pagination from '@/src/components/Pagination/Pagination';
 import RepoStats from '@/src/components/RepoStats/RepoStats';
 import IconIssue from '@/src/components/Icons/IconIssue';
 import Skeleton from '@/src/components/Skeleton/Skeleton';
@@ -160,9 +158,9 @@ export default {
   components: {
     TheText,
     RepoStats,
+    Pagination,
     RepoLang,
     IconIssue,
-    TheButton,
     Skeleton,
   },
   data() {
