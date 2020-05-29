269251c7c8edb5b0b90ef15087df2c4b1a02888b
Mikhail <mcheremuhin@ostrovok.ru>
2020-05-29 19:32:08 +0300

    I dont want to create the meaningfull message for commit

--- plugins/api.ts
+++ plugins/api.ts
@@ -17,6 +17,7 @@ import {
   RepositoryData,
   Repository,
 } from '@/src/interfaces/Repository/Repository';
+import { Provider } from '@/src/interfaces/Provider';
 import userResolver from '@/src/interfaces/User/resolver';
 import repositoryResolver from '@/src/interfaces/Repository/resolver';
 import { ProgrammingLanguage } from '@/src/interfaces/ProgrammingLanguage';
