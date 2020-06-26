9ec79cf9526f4bc847ded1eaeb1e57ac8379f7c7
Mikhail <mcheremuhin@ostrovok.ru>
2020-06-26 17:05:23 +0300

    Rename repository add page files

--- pages/repos/add/index.vue
+++ pages/repos/add/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section :class="$style.addRepo">
+  <section :class="styles.addRepo">
     <BlockWrapper size="tablet">
       <the-text tag="h1" size="h2">
         Add a new repository to OpenWiden
@@ -11,42 +11,42 @@
         with vsc at the same time
       </the-text>
 
-      <div :class="$style.addRepoBlock">
+      <div :class="styles.addRepoBlock">
         <the-text tag="h2" size="h3">
           GitLab
         </the-text>
 
         <div v-if="repos.loadingStatus === DATA_STATUS.LOADING">Loading</div>
 
-        <table v-else :class="$style.addRepoTable">
+        <table v-else :class="styles.addRepoTable">
           <tr
             v-for="repo in repos.data"
             :key="repo.name"
-            :class="$style.tableRow"
+            :class="styles.tableRow"
           >
-            <td :class="[$style.tableCell, $style.repoName]">
+            <td :class="[styles.tableCell, styles.repoName]">
               {{ repo.name }}
             </td>
 
-            <td :class="$style.tableCell">
+            <td :class="styles.tableCell">
               <repo-stats v-bind="repo" />
             </td>
 
-            <td :class="$style.tableCell">
-              <span v-if="repo.isAdded" :class="[$style.status, $style.added]">
+            <td :class="styles.tableCell">
+              <span v-if="repo.isAdded" :class="[styles.status, styles.added]">
                 Added
               </span>
               <span
                 v-if="repo.isLoading"
-                :class="[$style.status, $style.pending]"
+                :class="[styles.status, styles.pending]"
               >
                 Pending
               </span>
             </td>
 
-            <td :class="$style.tableCell">
+            <td :class="styles.tableCell">
               <the-button
-                :class="$style.button"
+                :class="styles.button"
                 :is-transparent="repo.isAdded"
                 :is-loading="repo.isLoading"
                 :disabled="repo.isLoading"
@@ -68,6 +68,7 @@
 </template>
 
 <script>
+import styles from './index.css?module';
 import TheText from '@/src/components/TheText/TheText';
 import BlockWrapper from '@/src/components/BlockWrapper/BlockWrapper';
 import RepoStats from '@/src/components/RepoStats/RepoStats';
@@ -87,6 +88,11 @@ export default {
       repos: DEFAULT_DATA_OBJECT,
       repoErr: null,
     };
+  },
+  computed: {
+    styles() {
+      return styles;
+    },
   },
   created() {
     this.getRepos();
@@ -117,9 +123,11 @@ export default {
       this.$api
         .addUserRepository(id)
         .then(() => {
-          this.repos.data.find((item, index) => {
-            if (item.id === id) {
-              this.$set(this.repos.data, index, { ...item, isLoading: true });
+          const { data } = this.repos;
+
+          data.find((item, index) => {
+            if (item.id !== id) {
+              this.$set(data, index, { ...item, isLoading: true });
             }
           });
         })
@@ -133,82 +141,3 @@ export default {
   },
 };
 </script>
-
-<style scoped module>
-.addRepo {
-  margin-top: 70px;
-}
-
-.addRepoBlock {
-  margin-top: var(--size-m);
-}
-
-.addRepoTable {
-  margin-top: var(--size-s);
-}
-
-.tableCell {
-  padding: 0;
-  padding-top: 10px;
-  padding-bottom: calc(var(--size-xxs) * 2);
-
-  &:first-of-type {
-    padding-left: calc(var(--size-s) * 2);
-  }
-
-  &:last-of-type {
-    padding-right: calc(var(--size-s) * 2);
-  }
-}
-
-.tableRow {
-  border-bottom: 1px solid #c4c4c4;
-
-  &:first-of-type .tableCell {
-    padding-top: 0;
-  }
-}
-
-.button {
-  width: 100%;
-  min-height: 36px;
-  max-width: 100px;
-  margin-left: auto;
-  font-size: 14px;
-  text-transform: uppercase;
-}
-
-.status {
-  font-size: 11px;
-  font-weight: 700;
-  text-transform: uppercase;
-  padding: calc(var(--size-xxs) * 2) var(--size-s);
-  border-radius: 4px;
-
-  &.added {
-    background-color: #53b124;
-  }
-
-  &.pending {
-    background-color: #fec400;
-  }
-
-  &.error {
-    background-color: #e25443;
-  }
-}
-
-.repoName {
-  text-transform: capitalize;
-}
-
-@media (min-width: 768px) {
-  .wrapper {
-    padding: var(--size-l);
-  }
-
-  .addRepoBlock {
-    margin-top: var(--size-l);
-  }
-}
-</style>
