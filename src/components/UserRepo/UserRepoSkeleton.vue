9e54b4a0aba078ca28affbfe566ffb4a521554e4
Mikhail Rerberg <m.rerberg@corp.mail.ru>
2020-09-21 13:22:04 +0300

    Add UserRepo component

---
+++ src/components/UserRepo/UserRepoSkeleton.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :class="[styles.repo]">
+    <div :class="styles.repoBlock">
+      <skeleton
+        :class="styles.repoVCS"
+        :style="{ width: '28px', height: '28px' }"
+        is-circle
+      />
+
+      <div :class="styles.repoName">
+        <skeleton :style="{ width: '8em' }" is-line />
+      </div>
+    </div>
+
+    <div :class="styles.repoBlock">
+      <skeleton :style="{ width: '8em' }" is-line />
+    </div>
+
+    <div :class="styles.repoBlock">
+      <skeleton :style="{ width: '6em' }" is-line />
+    </div>
+  </div>
+</template>
+
+<script>
+import styles from './UserRepo.css?module';
+import Skeleton from '@/src/components/Skeleton/Skeleton';
+
+export default {
+  components: {
+    Skeleton,
+  },
+
+  computed: {
+    styles() {
+      return styles;
+    },
+  },
+};
+</script>
