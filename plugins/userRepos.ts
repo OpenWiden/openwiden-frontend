f40bdd0072d63a64aef4b8e190eea7c16b1415a0
Mikhail Rerberg <m.rerberg@corp.mail.ru>
2020-11-29 20:42:55 +0300

    feat: add user repo plugin

---
+++ plugins/userRepos.ts
@@ -0,0 +1,15 @@
+import { Context } from '@nuxt/types';
+import { events } from '../src/components/UserRepos/events';
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $update: (props: any) => void;
+  }
+}
+
+export default function (_: Context, inject: any) {
+  inject('update', ({ object: { id, state } }: any) => {
+    // id, status
+    events.$emit('update', { id, state });
+  });
+}
