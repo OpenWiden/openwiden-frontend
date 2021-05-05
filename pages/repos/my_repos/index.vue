7bea185fbbe78db7589e6cdf765a863364a610cf
Mikhail Rerberg <m.rerberg@corp.mail.ru>
2021-05-05 15:59:02 +0300

    feat: add entry animation for 'my_repos' page

--- pages/repos/my_repos/index.vue
+++ pages/repos/my_repos/index.vue
@@ -1,38 +1,32 @@
 <template>
   <section :class="styles.addRepo">
     <block-wrapper size="tablet" :class="styles.wrapper">
-      <the-text tag="h1" size="h2" :with-underlay="true">
-        My repositories
-      </the-text>
+      <transition appear @before-enter="beforeEnter" @enter="enter">
+        <the-text tag="h1" size="h2" :with-underlay="true">
+          My repositories
+        </the-text>
+      </transition>
 
-      <the-text tag="p" :class="styles.subtitle">
-        You can add your repositories to OpenWiden from any connected version
-        control system. <br />
-        Also, you are able to have more than one active connection with vsc at
-        the same time.
-      </the-text>
+      <transition appear @before-enter="beforeEnter" @enter="enterP">
+        <the-text tag="p" :class="styles.subtitle">
+          You can add your repositories to OpenWiden from any connected version
+          control system. <br />
+          Also, you are able to have more than one active connection with vsc at
+          the same time.
+        </the-text>
+      </transition>
 
-      <user-repos
-        :loading-status="repos.loadingStatus === DATA_STATUS.LOADING"
-        :repos="repos.data"
-      />
-
-      <pagination
-        :pagination="pagination"
-        :wrapper-class="styles.pagination"
-        :buttons-class="styles.paginationControl"
-        :on-click="getRepos"
-      />
+      <user-repos />
     </block-wrapper>
   </section>
 </template>
 
 <script>
+import gsap from 'gsap';
 import styles from './index.css?module';
 import TheText from '@/src/components/TheText/TheText';
 import BlockWrapper from '@/src/components/BlockWrapper/BlockWrapper';
 import UserRepos from '@/src/components/UserRepos/UserRepos';
-import Pagination from '@/src/components/Pagination/Pagination';
 import { DATA_STATUS, DEFAULT_DATA_OBJECT } from '@/src/interfaces/Data';
 
 export default {
@@ -40,7 +34,6 @@ export default {
     TheText,
     BlockWrapper,
     UserRepos,
-    Pagination,
   },
   data() {
     return {
@@ -58,49 +51,22 @@ export default {
       return styles;
     },
   },
-  created() {
-    this.getRepos();
-  },
   methods: {
-    changeRepoStatus(id, nextStatus) {
-      const { data } = this.repos;
-
-      data.find((item, index) => {
-        if (item.id === id) {
-          this.$set(data, index, { ...item, status: nextStatus });
-        }
+    beforeEnter(el) {
+      el.style.transform = 'translateY(-20px)';
+      el.style.opacity = 0;
+    },
+    enter(el, done) {
+      gsap.to(el, { duration: 1, opacity: 1, y: 0, onComplete: done });
+    },
+    enterP(el, done) {
+      gsap.to(el, {
+        delay: 0.5,
+        duration: 1,
+        opacity: 1,
+        y: 0,
+        onComplete: done,
       });
-    },
-
-    onClick(status) {
-      return status === 'added' ? this.removeRepository : this.addRepository;
-    },
-
-    getRepos(url) {
-      const { repos, $api } = this;
-
-      repos.loadingStatus = DATA_STATUS.LOADING;
-
-      $api
-        .getUserRepositories(url)
-        .then((response) => {
-          const { results, next, previous } = response;
-
-          if (!results.length) {
-            repos.loadingStatus = DATA_STATUS.IDLE;
-            return;
-          }
-
-          repos.data = results.sort((a) => (a.status === 'added' ? -1 : 1));
-          repos.loadingStatus = DATA_STATUS.LOADED;
-
-          this.pagination.next = next;
-          this.pagination.previous = previous;
-        })
-        .catch((err) => {
-          repos.loadingStatus = DATA_STATUS.FAILED;
-          repos.errorText = err.message;
-        });
     },
   },
 };
